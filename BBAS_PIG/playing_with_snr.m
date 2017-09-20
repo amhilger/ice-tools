ecbbc7c53da19d02a6becdc545e6eed76114df72
amhilger <31424318+amhilger@users.noreply.github.com>
2017-09-20 15:18:55 -0700

    Add random sampling period offset to snr threshold test

--- BBAS_PIG/playing_with_snr.m
+++ BBAS_PIG/playing_with_snr.m
@@ -7,12 +7,9 @@ f_samp = 22e6; %sampling rate in Hertz
 A_max = 0.2551; %maximum abruptness
 D_r = 10;% number of samples in first return, typical case
 
-t = -T:1/f_samp:T; %time relative to t_o + t_c, sampled 
-s_out = K*T*exp(-1j*2*pi*K*t_c*t) .* sinc(K*T*t);
-idx_0 = (length(t)+1)/2;
-s_max = 10*log10(s_out(idx_0)); %maximum power
-s_agg = 10*log10( sum( s_out(idx_0 - D_r : idx_0 + D_r) ) );
-A_nonoise = 10^(0.1*(s_max-s_agg));
+
+
+
 
 %blackman windowing
 % black = blackman((length(s_out)-1)/2)';
@@ -21,29 +18,33 @@ A_nonoise = 10^(0.1*(s_max-s_agg));
 %A_ma
 
 
-plot_idx = idx_0 - 2*D_r : idx_0 + 2*D_r;
-snr = 0:2:32;
+
+snr = 0:2:26;
 
 num_trials = 1000;
 A = zeros(length(snr), num_trials);
 
-
-
 close(figure(1))
 for i = 1:length(snr)
     for j = 1:num_trials
-        s_noise = s_out + wgn(1, length(s_out), s_max - snr(i)).^2;
+        t = (-T:1/f_samp:T) + 1/f_samp*rand(); %time relative to t_o + t_c, sampled
+        %impulse response of ideal matched filter for chirp pulse
+        s_out = K*T*exp(-1j*2*pi*K*t_c*t) .* sinc(K*T*t);
+        [s_max, idx_0] = max(s_out);
+        P_max = 20*log10(abs(s_max)); %maximum power, dB
+        %add gaussian white noise
+        s_noise = s_out + wgn(1, length(s_out), P_max/2 - snr(i)).^2;
         
+        %perform detection on signal
         s_det = real(s_noise).^2 + imag(s_noise).^2;
 
         P_agg = 10*log10(sum(s_det(idx_0 - D_r : idx_0 + D_r)));
-        P_max = 10*log10(s_det(idx_0));
         A(i,j) = 10^(0.1*(P_max - P_agg));
 
-        % subplot(211); plot(t(plot_idx), abs(s_out(plot_idx))); 
-        % hold on;      plot(t(plot_idx), abs(s_noise(plot_idx)));
+        %plot the first trace for SNR = 4, 12, 20 ...
         if j == 1 && mod(i,4) == 3 
-            figure(1) 
+            figure(1)
+            plot_idx = idx_0 - 2*D_r : idx_0 + 2*D_r;
             plot(t(plot_idx), 10*log10(abs(s_det(plot_idx))) - 100);
             hold on;  
         end
@@ -52,6 +53,7 @@ end
 figure(1); legend('SNR = 4', 'SNR = 12', 'SNR = 20', 'SNR = 32')
 xlabel('time, s'), ylabel('Power, dB')
 
+%Plot peakiness relative to peakiness of signal with very high SNR
 figure(2); plot(snr, mean(A,2) / mean(A(end,:)) )
 xlabel('SNR, dB'); ylabel('Peakiness')
 
