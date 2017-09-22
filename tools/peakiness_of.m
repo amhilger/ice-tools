d1f215d4928d7269455805ccc708a3df392d7155
amhilger <andrewmhilger@gmail.com>
2017-09-22 12:06:36 -0700

    Cleanup peakiness comments

--- tools/peakiness_of.m
+++ tools/peakiness_of.m
@@ -1,18 +1,20 @@
 function [peakiness, rms_norm] = peakiness_of(abrupt_max, abrupt)
-%center frequency must be in Hz
+%{this function computes the peakiness (system independent abruptness)
+based on the max abruptness of the radar system and the abruptness observed 
+from a transect %}
 
+%abruptness and peakiness are a function of g, which is the wavelength-
+%normalized rms height multiplied by a scaling prefactor:  
+% g = @(rms, freq_ctr) 4*pi*rms*freq_ctr*n_ice/c_light;
 
 n_ice = 1.782;
 % c_light = 2.99792e8; %m/s
-% 
-% 
-% %scalar function where g is abbreviation in abruptness formula
-% g = @(rms, freq_ctr) 4*pi*rms*freq_ctr*n_ice/c_light;
+
 %reference values of g covering peakiness from 1 (g = 0) to 0.01 (g ~= 5)
 g_ref = 0:0.001:5;
 
 %determine the abruptness function of rms for reference radar (see Peters
-%05' for correct equation)
+%05' and TJ in prep)
 abrupt_rms_sys = abrupt_max*exp(-g_ref.^2) .* besseli(0,g_ref.^2/2).^2;
 %linearly scale reference abruptness to peakiness index
 peaky_ref = exp(-g_ref.^2) .* besseli(0,g_ref.^2/2).^2;
